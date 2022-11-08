45625bf9bcb33ccd7faaf8baa3733d72bb5182d4
Dang Quang Huy <[nhocbehue270@gmail.com]>
2022-11-08 22:59:43 +0700

    Fix handle collision for player and fix place bomb

--- src/main/java/uet/oop/bomberman/Player.java
+++ src/main/java/uet/oop/bomberman/Player.java
@@ -6,59 +6,50 @@ import uet.oop.bomberman.entities.Entity;
 import uet.oop.bomberman.entities.LayeredEntity;
 import uet.oop.bomberman.entities.animatedEntities.character.Bomber;
 import uet.oop.bomberman.entities.destroyable.Brick;
+import uet.oop.bomberman.entities.destroyable.bomb.BombExplosionNormal;
+import uet.oop.bomberman.entities.destroyable.bomb.BombExplosionPro;
 import uet.oop.bomberman.entities.destroyable.items.BombItem;
 import uet.oop.bomberman.entities.destroyable.items.FlameItem;
 import uet.oop.bomberman.entities.destroyable.items.HeartItem;
 import uet.oop.bomberman.entities.destroyable.items.SpeedItem;
-import uet.oop.bomberman.entities.destroyable.bomb.BombExplosionNormal;
-import uet.oop.bomberman.entities.destroyable.bomb.BombExplosionPro;
 import uet.oop.bomberman.entities.undestroyable.Grass;
 import uet.oop.bomberman.entities.undestroyable.Portal;
 import uet.oop.bomberman.graphics.Sprite;
-import uet.oop.bomberman.sound.Sound;
 
 import java.util.Set;
 
 import static javafx.scene.input.KeyCode.*;
 
 public class Player {
+    // Đếm số player
+    private static short count = 0;
     // Nhân vật Bomber
     private Bomber bomberman;
-
     // Lưu điểm
     private int mark;
     // Lưu số mạng của nhân vật
     private int heart;
-
     // Số lượng bomb nhân vật có
     private int bomb;
-
     // Xem có đang ở bomb nạp VIP ko
     private boolean isBombPro;
-
     // Lưu trữ map để sử lý check map khi di chuyển nhanh hơn
     private Character[][] map;
-
     // Lưu trạng thái xem có đang tăng tốc độ hay không
     // để sử lý tình trạng tăng tốc độ dẫn đến đi xuyên tường
     private boolean isSetSpeed;
-
     // Lưu xem nhan vật đi theo hướng nào
     // true là đi theo chiều ngang
     // false là đi theo chiều dọc
     private boolean direction;
-
     // Lưu thứ tự của player
     private short num;
-
-    // Đếm số player
-    private static short count = 0;
-
     // Lưu thứ tự lấy sprite
     private short numBomberman;
 
     // Lưu xem tăng hay giảm speed
     private boolean isIncreaseSpeed;
+
     public Player(Character[][] map, short numBomberman, int heart, int bomb) {
         bomberman = new Bomber(1, 1, Bomber.player_right[numBomberman], numBomberman);
         this.map = map;
@@ -68,7 +59,7 @@ public class Player {
         this.bomb = bomb;
         this.isBombPro = false;
         this.isSetSpeed = false;
-        this.num = (++ this.count);
+        this.num = (++this.count);
     }
 
     public short getNumBomberman() {
@@ -77,10 +68,11 @@ public class Player {
 
     /**
      * Sử lý sự kiện từ bàn phím để điều khiển bomber
+     *
      * @param codes mã code của phím
      */
     public void play(Set<KeyCode> codes) {
-        if(!bomberman.isAlive()){
+        if (!bomberman.isAlive()) {
             return;
         }
 
@@ -130,11 +122,6 @@ public class Player {
         eatItem();
     }
 
-    private void setSpeed(boolean increase) {
-        bomberman.setSpeed(increase);
-        isSetSpeed = false;
-    }
-
     /**
      * Kiểm tra và ăn các item hiện ra khi phá tưởng
      */
@@ -154,23 +141,17 @@ public class Player {
                      */
                     mark += 50;
                     isSetSpeed = true;
-                }
-
-                else if (((LayeredEntity) entity).getTopEntity() instanceof FlameItem) {
+                } else if (((LayeredEntity) entity).getTopEntity() instanceof FlameItem) {
                     ((LayeredEntity) entity).removeTop();
                     /**
                      * to do something
                      */
                     mark += 50;
                     isBombPro = true;
-                }
-
-                else if (((LayeredEntity) entity).getTopEntity() instanceof HeartItem) {
+                } else if (((LayeredEntity) entity).getTopEntity() instanceof HeartItem) {
                     mark += 50;
                     heart++;
-                }
-
-                else if (((LayeredEntity) entity).getTopEntity() instanceof BombItem) {
+                } else if (((LayeredEntity) entity).getTopEntity() instanceof BombItem) {
                     mark += 50;
                     bomb++;
                 }
@@ -181,6 +162,7 @@ public class Player {
     /**
      * Kiểm tra xem bomber đã đi vào cổng chưa
      * Nếu đi vào cổng thì chiến thắng
+     *
      * @return
      */
     private boolean checkVictory() {
@@ -203,8 +185,8 @@ public class Player {
     }
 
     public void update() {
-        if(!bomberman.isAlive()
-                && bomberman.getAnimate_die() == bomberman.getTime_die()){
+        if (!bomberman.isAlive()
+                && bomberman.getAnimate_die() == bomberman.getTime_die()) {
             if (heart > 1) {
                 bomberman.setIsAlive(true);
                 heart--;
@@ -224,6 +206,7 @@ public class Player {
         }
         bomberman.update();
     }
+
     public void render(GraphicsContext gc) {
         bomberman.render(gc);
     }
@@ -232,14 +215,14 @@ public class Player {
      * Them bom vao vi tri đang đứng
      *
      * @param pro is true thì đặt bom nạp vip
-     *        pro is false thì đặt bom loại bình thường
+     *            pro is false thì đặt bom loại bình thường
      */
     private void addBomb(boolean pro) {
-        int x = bomberman.getX() / Sprite.SCALED_SIZE;
-        int y = bomberman.getY() / Sprite.SCALED_SIZE;
+        int x = (bomberman.getX() + Sprite.SCALED_SIZE / 2) / Sprite.SCALED_SIZE;
+        int y = (bomberman.getY() + Sprite.SCALED_SIZE - 1) / Sprite.SCALED_SIZE;
         map[y][x] = 'b';
-        if(!pro)
-            Management.bombs.add(new BombExplosionNormal(x, y, null, num ,map));
+        if (!pro)
+            Management.bombs.add(new BombExplosionNormal(x, y, null, num, map));
         else
             Management.bombs.add(new BombExplosionPro(x, y, null, num, map));
     }
@@ -252,52 +235,95 @@ public class Player {
     // left : 4
     private boolean checkMap(int s) {
 
-        int x = bomberman.getX() / Sprite.SCALED_SIZE;
-        int y = bomberman.getY() / Sprite.SCALED_SIZE;
+//        int x = bomberman.getX() / Sprite.SCALED_SIZE;
+//        int y = bomberman.getY() / Sprite.SCALED_SIZE;
+        int x = bomberman.getX();
+        int y = bomberman.getY();
+
+//        if (s == 1) {
+//            if (x * Sprite.SCALED_SIZE != bomberman.getX()) {
+//                return false;
+//            }
+//            if (y * Sprite.SCALED_SIZE == bomberman.getY()) {
+//                return Character.valueOf(map[y - 1][x]).compareTo('#') != 0 &&
+//                        Character.valueOf(map[y - 1][x]).compareTo('*') != 0;
+//            }
+//        }
+//
+//        if (s == 2) {
+//            if (y * Sprite.SCALED_SIZE != bomberman.getY()) {
+//                return false;
+//            }
+//
+//            if (x * Sprite.SCALED_SIZE == bomberman.getX()) {
+//                return Character.valueOf(map[y][x + 1]).compareTo('#') != 0 &&
+//                        Character.valueOf(map[y][x + 1]).compareTo('*') != 0;
+//            }
+//        }
+//
+//        if (s == 3) {
+//            if (x * Sprite.SCALED_SIZE != bomberman.getX()) {
+//                return false;
+//            }
+//            if (y * Sprite.SCALED_SIZE == bomberman.getY()) {
+//                return Character.valueOf(map[y + 1][x]).compareTo('#') != 0 &&
+//                        Character.valueOf(map[y + 1][x]).compareTo('*') != 0;
+//            }
+//        }
+//
+//        if (s == 4) {
+//            if (y * Sprite.SCALED_SIZE != bomberman.getY()) {
+//                return false;
+//            }
+//
+//            if (x * Sprite.SCALED_SIZE == bomberman.getX()) {
+//                return Character.valueOf(map[y][x - 1]).compareTo('#') != 0 &&
+//                        Character.valueOf(map[y][x - 1]).compareTo('*') != 0;
+//            }
+//        }
+
+        int speed = bomberman.getSpeed();
+
 
         if (s == 1) {
-            if(x * Sprite.SCALED_SIZE != bomberman.getX()) {
-                return false;
-            }
-            if (y * Sprite.SCALED_SIZE == bomberman.getY()) {
-                return Character.valueOf(map[y - 1][x]).compareTo('#') != 0 &&
-                        Character.valueOf(map[y - 1][x]).compareTo('*') != 0;
-            }
-        }
+            int xFuture1 = x / Sprite.SCALED_SIZE;
+            int yFuture1 = (y - speed) / Sprite.SCALED_SIZE;
+            int xFuture2 = (x + Sprite.SCALED_SIZE - 1) / Sprite.SCALED_SIZE;
+            int yFuture2 = (y - speed) / Sprite.SCALED_SIZE;
 
+            return !isBlock(map[yFuture1][xFuture1]) && !isBlock(map[yFuture2][xFuture2]);
+        }
         if (s == 2) {
-            if (y * Sprite.SCALED_SIZE != bomberman.getY()){
-                return false;
-            }
+            int xFuture1 = (x + speed + Sprite.SCALED_SIZE - 1) / Sprite.SCALED_SIZE;
+            int yFuture1 = y / Sprite.SCALED_SIZE;
+            int xFuture2 = (x + speed + Sprite.SCALED_SIZE - 1) / Sprite.SCALED_SIZE;
+            int yFuture2 = (y + Sprite.SCALED_SIZE - 1) / Sprite.SCALED_SIZE;
 
-            if(x * Sprite.SCALED_SIZE == bomberman.getX()){
-                return Character.valueOf(map[y][x + 1]).compareTo('#') != 0 &&
-                        Character.valueOf(map[y][x + 1]).compareTo('*') != 0;
-            }
+            return !isBlock(map[yFuture1][xFuture1]) && !isBlock(map[yFuture2][xFuture2]);
         }
-
         if (s == 3) {
-            if(x * Sprite.SCALED_SIZE != bomberman.getX()) {
-                return false;
-            }
-            if (y * Sprite.SCALED_SIZE == bomberman.getY()) {
-                return Character.valueOf(map[y + 1][x]).compareTo('#') != 0 &&
-                        Character.valueOf(map[y + 1][x]).compareTo('*') != 0;
-            }
-        }
+            int xFuture1 = x / Sprite.SCALED_SIZE;
+            int yFuture1 = (y + speed + Sprite.SCALED_SIZE - 1) / Sprite.SCALED_SIZE;
+            int xFuture2 = (x + Sprite.SCALED_SIZE - 1) / Sprite.SCALED_SIZE;
+            int yFuture2 = (y + speed + Sprite.SCALED_SIZE - 1) / Sprite.SCALED_SIZE;
 
+            return !isBlock(map[yFuture1][xFuture1]) && !isBlock(map[yFuture2][xFuture2]);
+        }
         if (s == 4) {
-            if (y * Sprite.SCALED_SIZE != bomberman.getY()){
-                return false;
-            }
+            int xFuture1 = (x - speed) / Sprite.SCALED_SIZE;
+            int yFuture1 = y / Sprite.SCALED_SIZE;
+            int xFuture2 = (x - speed) / Sprite.SCALED_SIZE;
+            int yFuture2 = (y + Sprite.SCALED_SIZE - 1) / Sprite.SCALED_SIZE;
 
-            if(x * Sprite.SCALED_SIZE == bomberman.getX()){
-                return Character.valueOf(map[y][x - 1]).compareTo('#') != 0 &&
-                        Character.valueOf(map[y][x - 1]).compareTo('*') != 0;
-            }
+            return !isBlock(map[yFuture1][xFuture1]) && !isBlock(map[yFuture2][xFuture2]);
         }
+
 
         return true;
+    }
+
+    private boolean isBlock(Character character) {
+        return character.compareTo('#') == 0 || character.compareTo('*') == 0;
     }
 
     // Trả về bombers
@@ -319,6 +345,11 @@ public class Player {
 
     public int getSpeed() {
         return bomberman.getSpeed();
+    }
+
+    private void setSpeed(boolean increase) {
+        bomberman.setSpeed(increase);
+        isSetSpeed = false;
     }
 
     public int getBomb() {
