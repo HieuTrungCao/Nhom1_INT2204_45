d8dbaa64c02d24654fcb3f74f1127a34c321d380
HieuTRungCao <21021490@vnu.edu.vn>
2022-11-09 21:40:34 +0700

    modify SpeedItem.java

--- src/main/java/uet/oop/bomberman/entities/destroyable/items/SpeedItem.java
+++ src/main/java/uet/oop/bomberman/entities/destroyable/items/SpeedItem.java
@@ -1,9 +1,10 @@
 package uet.oop.bomberman.entities.destroyable.items;
 
+import uet.oop.bomberman.entities.Entity;
 import uet.oop.bomberman.entities.destroyable.DestroyableEntities;
 import uet.oop.bomberman.graphics.Sprite;
 
-public class SpeedItem extends DestroyableEntities {
+public class SpeedItem extends Entity {
     public SpeedItem(int xUnit, int yUnit, Sprite sprite) {
         super(xUnit, yUnit, sprite);
     }
